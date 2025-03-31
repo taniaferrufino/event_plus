591a6dffce6d4340eca4407b5ccb76f9d3b5e10c
taniaferrufino <142746006+taniaferrufino@users.noreply.github.com>
2025-03-30 20:00:37 -0600

    creando proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientesModule } from './modules/clientes/clientes.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClientesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
